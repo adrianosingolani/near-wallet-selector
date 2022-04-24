a110b47449e8a955d1cf413fdb54e3ff0b6e6fa6
Adriano Singolani <adrianosingolani@gmail.com>
2022-04-24 15:10:28 -0300

    guest-book clone. Show message date and time. Few design changes

---
+++ src/components/Messages.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+
+export default function Messages({ messages }) {
+  useEffect(() => {
+    console.log(messages);
+  }, [messages])
+  
+
+  return (
+    <>
+      <h2>Messages</h2>
+      {messages.map((message, i) =>
+        // TODO: format as cards, add timestamp
+        <p key={i} className={message.premium ? 'is-premium' : ''}>
+          <span style={{fontSize: '10px', fontStyle: 'italic'}}>
+            {moment(message.datetime/1000000).format('YYYY-MM-DD HH:mm:ss')}
+          </span><br />
+
+          <strong>{message.sender}</strong>:<br/>
+
+          {message.text}
+        </p>
+      )}
+    </>
+  );
+}
+
+Messages.propTypes = {
+  messages: PropTypes.array
+};
